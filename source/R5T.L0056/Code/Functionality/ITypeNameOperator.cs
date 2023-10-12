232c853a185210d0afe2b311b0294f7427c78bf7
davidcoats <david@getsafetycone.com>
2023-10-11 22:36:24 -0700

    Interim changes.

---
+++ source/R5T.L0056/Code/Functionality/ITypeNameOperator.cs
@@ -0,0 +1,60 @@
+using System;
+
+using R5T.T0132;
+
+using R5T.N0000;
+
+
+namespace R5T.L0056
+{
+    [FunctionalityMarker]
+    public partial interface ITypeNameOperator : IFunctionalityMarker,
+        L0053.ITypeNameOperator
+    {
+        public new string Append_ElementTypeRelationshipMarker(
+            Type elementTypeParentType,
+            string elementTypeNamespacedTypeName)
+        {
+            var elementTypeRelationship = Instances.ElementTypeRelationshipOperator.Get_ElementTypeRelationship(elementTypeParentType);
+
+            // Has no known relationship.
+            if (elementTypeRelationship == ElementTypeRelationships.None)
+            {
+                throw Instances.ExceptionOperator.Get_UnknownElementTypeRelationshipException();
+            }
+
+            var output = elementTypeNamespacedTypeName;
+
+            // Is the type an array type?
+            var isArray = Instances.FlagsOperator.Has_Flag(
+                elementTypeRelationship,
+                ElementTypeRelationships.Array);
+
+            if (isArray)
+            {
+                output += Instances.TypeNameAffixes.Array_Suffix;
+            }
+
+            var isReference = Instances.FlagsOperator.Has_Flag(
+                elementTypeRelationship,
+                ElementTypeRelationships.Reference);
+
+            if (isReference)
+            {
+                output += Instances.TypeNameAffixes.ByReference_Suffix;
+            }
+
+            var isByPointer = Instances.FlagsOperator.Has_Flag(
+                elementTypeRelationship,
+                ElementTypeRelationships.Reference);
+
+            if (isByPointer)
+            {
+                output += Instances.TypeNameAffixes.Pointer_Suffix;
+                return output;
+            }
+
+            return output;
+        }
+    }
+}
--- source/R5T.L0056/Code/Instances.cs
+++ source/R5T.L0056/Code/Instances.cs
@@ -6,10 +6,14 @@ namespace R5T.L0056
     public static class Instances
     {
         public static L0053.IAssemblyFileNameOperator AssemblyFileNameOperator_Platform => L0053.AssemblyFileNameOperator.Instance;
+        public static T0221.IElementTypeRelationshipOperator ElementTypeRelationshipOperator => T0221.ElementTypeRelationshipOperator.Instance;
+        public static L0053.IExceptionOperator ExceptionOperator => L0053.ExceptionOperator.Instance;
         public static IExecutablePathOperator ExecutablePathOperator => L0056.ExecutablePathOperator.Instance;
         public static IFileExtensions FileExtensions => L0056.FileExtensions.Instance;
         public static IFileNameOperator FileNameOperator => L0056.FileNameOperator.Instance;
         public static L0053.IFileNameOperator FileNameOperator_Platform => L0053.FileNameOperator.Instance;
+        public static L0053.IFlagsOperator FlagsOperator => L0053.FlagsOperator.Instance;
         public static Extensions.IStringOperator StringOperator_Extensions => Extensions.StringOperator.Instance;
+        public static L0053.ITypeNameAffixes TypeNameAffixes => L0053.TypeNameAffixes.Instance;
     }
 }
